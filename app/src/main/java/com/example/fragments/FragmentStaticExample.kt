3274c6b2b199cf0128bbd0f7043b3352bc31c36f
vajra <kowtilyavajra@gmail.com>
2024-03-05 12:38:50 +0530

    Final Commit

--- app/src/main/java/com/example/fragments/FragmentStaticExample.kt
+++ app/src/main/java/com/example/fragments/FragmentStaticExample.kt
@@ -3,9 +3,16 @@ package com.example.fragments
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class FragmentStaticExample : AppCompatActivity() {
+
+class FragmentStaticExample : AppCompatActivity(), OnButtonPressListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment_static_example)
+    }
+
+    override fun onButtonPressed(msg: String){
+
+        var obj: BlankFragmentTwo = supportFragmentManager.findFragmentById(R.id.fragment2) as BlankFragmentTwo
+        obj.onFragmentInteraction(msg)
     }
 }
